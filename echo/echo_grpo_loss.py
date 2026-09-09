328229bf67974d5575dd8949e8dc98968aba8666
Ethan Kim <ethankim8683@gmail.com>
2026-09-09 04:50:57 -0700

    Write ECHO main.py

--- echo/echo_grpo_loss.py
+++
@@ -1,5 +0,0 @@
-from skyrl.backends.skyrl_train.utils.ppo_utils import register_policy_loss
-
-@register_policy_loss("echo_grpo")
-def compute_echo_grpo_loss():
-    pass
---
+++ echo/run.sh
@@ -0,0 +1,4 @@
+set -x
+
+uv run --isolated --extra fsdp -m main
+# TODO: figure out params
